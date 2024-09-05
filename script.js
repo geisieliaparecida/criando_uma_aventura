d9870e2d5d5e8a012bbb9c4d0222befbe08bc56f
geisieli <00001113387932sp@al.educacao.sp.gov.br>
2024-09-05 16:31:07 -0300

    desnvolvendo arquivos js, html e css

---
+++ script.js
@@ -0,0 +1,10 @@
+const avanca = document.querySelectorAll9('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('clik',function (){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso ='passo-' + this.getAttribute( 'date-proximo');
+
+        atual.classList
+    })
+})
